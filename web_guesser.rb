05179992e3adea63ed6fed4f7f9e31b9788ccb92
Vlad <vlad@hotmail.co.nz>
2017-06-29 18:59:04 -0700

    added some functionality to my web guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,36 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-generate = rand(0..100)
+number = rand(0..100)
 
-get '/' do 
-   erb :index
+get '/' do
+   
+   guess = params['guess'] 
+   message = msg(guess,number)
+   erb :index, :locals => {:number => number, :message => message}
+
+end 
+
+
+
+def msg(guess,number)
+ 	if guess != nil 
+ 		guess = guess.to_i
+ 		if guess == number 
+ 			"The number is: #{guess}"
+ 		elsif guess > number 
+ 			if guess - number > 5 
+ 				"#{guess} is way too high"
+ 			else 
+ 				"#{guess} is high"
+ 			end
+ 			else 
+ 				if number - guess > 5 
+ 					"#{guess} is way too low"
+ 				else
+ 					"#{guess} is low"
+ 			end   	
+ 		end 
+ 	end
+
 end 
