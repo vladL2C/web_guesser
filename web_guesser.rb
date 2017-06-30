29f326d252d196814e3cbfca643540b29b164158
Vlad <vlad@hotmail.co.nz>
2017-06-29 21:47:27 -0700

    added counter

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,15 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(0..100)
+@@remaining = 6
 
-get '/' do
-   
+get '/' do 
    guess = params['guess'] 
    message = msg(guess,number)
+   remaining = remainder
    color = get_color(guess,number)
-   erb :index, :locals => {:number => number, :message => message, :color => color}
+   erb :index, :locals => {:number => number, 
+   	:message => message, :color => color, 
+   	:remaining => remaining}
 
 end
+
+def remainder
+	@@remaining = @@remaining - 1 
+	"you have #{@@remaining} guesses remaining!"
+end 
+
+
 
 def get_color(guess, number)
 	guess = guess.to_i
