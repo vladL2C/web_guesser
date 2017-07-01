cce394813bff565ed415304a6d386d60ef5b3a85
Vlad <vlad@hotmail.co.nz>
2017-07-01 03:07:39 -0700

    re doing this

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,57 +1,30 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 
-number = rand(0..100)
-@@remaining = 6
+number = rand(1..100)
+
 
 get '/' do 
-   guess = params['guess'] 
-   message = msg(guess,number)
-   remaining = remainder
-   color = get_color(guess,number)
-   erb :index, :locals => {:number => number, 
-   	:message => message, :color => color, 
-   	:remaining => remaining}
-
-end
-
-def remainder
-	@@remaining = @@remaining - 1 
-	"you have #{@@remaining} guesses remaining!"
+guess = params['guess'].to_i
+message = get_message(guess,number)
+erb :index, :locals => {:number => number,:message => message}
 end 
 
 
-
-def get_color(guess, number)
-	guess = guess.to_i
-	if guess > number + 5 
-		"red"
-	elsif guess == number 
-		"green"	
+def get_message(guess,number)
+	if guess > number 
+		if guess > number + 5 
+			"Way too high"
+		else 
+			"too high"
+		end
+		 elsif guess < number 
+		  if guess < number -5 
+		  	"Way too low"
+		  else 
+		  		"too low"
+		  end 
+		 elsif guess == number 
+		  	"You guessed it: #{number}"			  
 	end 
-end 
-
-
-
-
-def msg(guess,number)
- 	if guess != nil 
- 		guess = guess.to_i
- 		if guess == number 
- 			"You Guessed it! #{guess}"
- 		elsif guess > number 
- 			if guess - number > 5 
- 				"#{guess} is way too high"
- 			else 
- 				"#{guess} is high"
- 			end
- 			else 
- 				if number - guess > 5 
- 					"#{guess} is way too low"
- 				else
- 					"#{guess} is low"
- 			end   	
- 		end 
- 	end
-
 end 
