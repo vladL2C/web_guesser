0d8679f5796da7a318fdf2896d7081edb2c19337
Vlad <vlad@hotmail.co.nz>
2017-07-02 21:24:20 -0700

    refactored a bit

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,28 +3,54 @@ require 'sinatra/reloader' if development?
 
 number = rand(1..100)
 
-
-get '/' do 
+get '/' do
 guess = params['guess'].to_i
-message = get_message(guess,number)
-erb :index, :locals => {:number => number,:message => message}
+cheater = params['cheat']
+color = colorize(guess,number)
+check = check_guess(guess,number,cheater)
+erb :index, :locals => {:number => number,:check => check,
+:color => color}
+end
+
+def colorize(guess,number)
+	if guess > number + 5 || guess < number - 5
+	   "red"
+	elsif guess > number || guess < number
+		"yellow"
+	else 
+	"green"	   	
+	end  
 end 
 
+def check_guess(guess,number,cheater)
+	if cheater == "true"
+		message = "The number is #{number} you cheater"
+	else 
+	get_message(guess,number)	
+	end 
+
+end 
 
 def get_message(guess,number)
+
 	if guess > number 
 		if guess > number + 5 
 			"Way too high"
 		else 
-			"too high"
+			"Too high"
 		end
 		 elsif guess < number 
 		  if guess < number -5 
 		  	"Way too low"
 		  else 
-		  		"too low"
+		  		"Too low"
 		  end 
 		 elsif guess == number 
-		  	"You guessed it: #{number}"			  
+		 	"you guessed it: #{number}"	
+		  			  
 	end 
+ 
 end 
+
+
+
